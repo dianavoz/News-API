aff66ad93d6722e692c3d9e5122dad093159163f
Diana Vozanu <you@example.com>
2022-03-29 07:59:35 +0100

    tests

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,9 +1,23 @@
-const { selectTopics } = require("../models/topics.models.js");
+const {
+  selectTopics,
+  selectArticlesById,
+} = require("../models/topics.models.js");
 
 exports.getTopics = (req, res) => {
   selectTopics()
     .then((topics) => {
       res.status(200).send({ topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  selectArticlesById(article_id)
+    .then((articles) => {
+      res.status(200).send({ articles });
     })
     .catch((err) => {
       next(err);
--- models/topics.models.js
+++ models/topics.models.js
@@ -5,3 +5,14 @@ exports.selectTopics = () => {
     return result.rows;
   });
 };
+
+exports.selectArticlesById = (article_id) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+    .then((article) => {
+      if (article.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not Found!" });
+      }
+      return article.rows[0];
+    });
+};
