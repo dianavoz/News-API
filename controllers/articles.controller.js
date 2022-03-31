e69dd9f9a1dd9e541b742f2902f17d41ad8aeec5
Diana Vozanu <you@example.com>
2022-03-31 13:42:42 +0100

    controller

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,6 +1,7 @@
 const {
   fetchArticleById,
   updateArticle,
+  fetchArticles,
 } = require("../models/ articles.model");
 
 exports.getArticleById = async (req, res, next) => {
@@ -21,6 +22,18 @@ exports.patchArticle = async (req, res, next) => {
     const article = await updateArticle(article_id, req.body);
 
     res.status(200).send({ article });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getArticles = async (req, res, next) => {
+  try {
+    const { sort_by } = req.query;
+
+    const articles = await fetchArticles(sort_by);
+
+    res.status(200).send({ articles });
   } catch (err) {
     next(err);
   }
