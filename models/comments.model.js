793ab13d138da6f3c34716c43a6f36585cc2eae8
Diana Vozanu <you@example.com>
2022-03-31 17:55:29 +0100

    comments model

--- models/comments.model.js
+++ models/comments.model.js
@@ -2,12 +2,17 @@ const db = require("../db/connection");
 
 exports.fetchCommentsByArticle = async (article_id) => {
   const comments = await db.query(
-    `SELECT comment_id, comments.votes, comments.created_at, users.name AS author, comments.body
-    FROM comments
-    JOIN articles ON comments.article_id = articles.article_id
-    JOIN users ON articles.author = users.username
-    WHERE articles.article_id = $1;`,
+    `SELECT comment_id,
+      comments.votes,
+      comments.created_at,
+      users.name AS author,
+      comments.body
+      FROM comments
+      JOIN articles ON comments.article_id = articles.article_id
+      JOIN users ON articles.author = users.username
+      WHERE articles.article_id = $1;`,
     [article_id]
   );
+
   return comments.rows;
 };
