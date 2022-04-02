0f1a37db77dd0f71b44916e0f7e6f555f8d71c48
Diana Vozanu <you@example.com>
2022-04-02 18:53:14 +0100

    added queries for order and topic filter

--- models/articles.model.js
+++ models/articles.model.js
@@ -37,19 +37,35 @@ exports.updateArticle = async (article_id, newVotes) => {
   return article.rows[0];
 };
 
-exports.fetchArticles = async (sort_by = `created_at`) => {
-  const sql = `
+exports.fetchArticles = async (
+  sort_by = "created_at",
+  order = "DESC",
+  topic
+) => {
+  sort_by = sort_by.toLowerCase();
+  order = order.toUpperCase();
+
+  let queryValues = [];
+
+  let queryStr = `
   SELECT name AS author,
    COUNT(comments.body)::int AS comment_count,
    articles.title,
    articles.article_id,
-   topic,articles.created_at,
+   articles.topic,articles.created_at,
    articles.votes
    FROM articles
    JOIN users ON articles.author=users.username
    JOIN comments ON articles.article_id=comments.article_id
-   GROUP BY articles.article_id,name
-   ORDER BY ${sort_by} DESC;`;
+   `;
+
+  if (topic) {
+    queryStr += `WHERE articles.topic=$1`;
+    queryValues.push(topic);
+  }
+
+  queryStr += `GROUP BY articles.article_id,name
+   ORDER BY ${sort_by} ${order};`;
 
   const validColumns = [
     "title",
@@ -63,7 +79,19 @@ exports.fetchArticles = async (sort_by = `created_at`) => {
     return Promise.reject({ status: 400, msg: "Invalid sort_by" });
   }
 
-  const articles = await db.query(sql);
+  if (!["DESC", "ASC"].includes(order)) {
+    return Promise.reject({ status: 400, msg: "Invalid order" });
+  }
+
+  if (!["DESC", "ASC"].includes(order)) {
+    return Promise.reject({ status: 400, msg: "Invalid order" });
+  }
+
+  const articles = await db.query(queryStr, queryValues);
+
+  if (articles.rows.length === 0) {
+    return Promise.reject({ status: 404, msg: "Topic doesn't exist" });
+  }
 
   return articles.rows;
 };
